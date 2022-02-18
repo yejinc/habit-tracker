4002c9fd3d9f4915c1f6d4c5a7bf00e05e3377af
Jinnie Cho <yejincho1105@gmail.com>
2022-02-19 00:02:58 +0900

    🥅 Managed key prop warning with assigning id

--- src/components/HabitList.jsx
+++ src/components/HabitList.jsx
@@ -4,16 +4,16 @@ import HabitItem from "./HabitItem";
 export default class HabitList extends Component {
   state = {
     habits: [
-      { name: "meditation", count: 0 },
-      { name: "spazieren gehen", count: 0 },
-      { name: "journaling", count: 0 },
+      { id: 1, name: "meditation", count: 0 },
+      { id: 2, name: "spazieren gehen", count: 0 },
+      { id: 3, name: "journaling", count: 0 },
     ],
   };
   render() {
     return (
       <ul>
         {this.state.habits.map((habit) => (
-          <HabitItem habit={habit} />
+          <HabitItem key={habit.id} habit={habit} />
         ))}
       </ul>
     );
