31afcbebe8e0386423ef73be4544548f7fafee86
Jinnie Cho <yejincho1105@gmail.com>
2022-02-24 19:09:02 +0900

    🎨 Exported states and functions to App.jsx and framed basic form for habit-form

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,48 @@
+import React, { Component } from "react";
 import "./App.css";
 import HabitList from "./components/HabitList";
 
-function App() {
-  return <HabitList />;
-}
+export default class App extends Component {
+  state = {
+    habitItemInfos: [
+      { id: 1, name: "meditation", count: 0 },
+      { id: 2, name: "spazieren gehen", count: 0 },
+      { id: 3, name: "journaling", count: 0 },
+    ],
+  };
 
-export default App;
+  handleIncrement = (habitItemInfo) => {
+    const habitItemInfos = [...this.state.habitItemInfos];
+    const index = habitItemInfos.indexOf(habitItemInfo);
+    habitItemInfos[index].count++;
+    this.setState({ habitItemInfos });
+  };
+
+  handleDecrement = (habitItemInfo) => {
+    const habitItemInfos = [...this.state.habitItemInfos];
+    const index = habitItemInfos.indexOf(habitItemInfo);
+    const count = habitItemInfos[index].count - 1;
+    habitItemInfos[index].count = count < 0 ? 0 : count;
+    this.setState({ habitItemInfos });
+  };
+
+  handleDelete = (habitItemInfo) => {
+    const habitItemInfos = this.state.habitItemInfos.filter(
+      (item) => item.id !== habitItemInfo.id
+    );
+    this.setState({ habitItemInfos });
+  };
+
+  render() {
+    return (
+      <>
+        <HabitList
+          habitItemInfos={this.state.habitItemInfos}
+          onIncrement={this.handleIncrement}
+          onDecrement={this.handleDecrement}
+          onDelete={this.handleDelete}
+        />
+      </>
+    );
+  }
+}
--- src/components/HabitList.jsx
+++ src/components/HabitList.jsx
@@ -2,49 +2,25 @@ import React, { Component } from "react";
 import HabitItem from "./HabitItem";
 
 export default class HabitList extends Component {
-  state = {
-    habitItemInfos: [
-      { id: 1, name: "meditation", count: 0 },
-      { id: 2, name: "spazieren gehen", count: 0 },
-      { id: 3, name: "journaling", count: 0 },
-    ],
-  };
-
-  onIncrement = (habitItemInfo) => {
-    const habitItemInfos = [...this.state.habitItemInfos];
-    const index = habitItemInfos.indexOf(habitItemInfo);
-    habitItemInfos[index].count++;
-    this.setState({ habitItemInfos });
-  };
-
-  onDecrement = (habitItemInfo) => {
-    const habitItemInfos = [...this.state.habitItemInfos];
-    const index = habitItemInfos.indexOf(habitItemInfo);
-    const count = habitItemInfos[index].count - 1;
-    habitItemInfos[index].count = count < 0 ? 0 : count;
-    this.setState({ habitItemInfos });
-  };
-
-  onDelete = (habitItemInfo) => {
-    const habitItemInfos = this.state.habitItemInfos.filter(
-      (item) => item.id !== habitItemInfo.id
-    );
-    this.setState({ habitItemInfos });
-  };
-
   render() {
     return (
-      <ul>
-        {this.state.habitItemInfos.map((habitItemInfo) => (
-          <HabitItem
-            key={habitItemInfo.id}
-            habitItemInfo={habitItemInfo}
-            onIncrement={this.onIncrement}
-            onDecrement={this.onDecrement}
-            onDelete={this.onDelete}
-          />
-        ))}
-      </ul>
+      <div className="habit-list" action="">
+        <ul>
+          {this.props.habitItemInfos.map((habitItemInfo) => (
+            <HabitItem
+              key={habitItemInfo.id}
+              habitItemInfo={habitItemInfo}
+              onIncrement={this.props.onIncrement}
+              onDecrement={this.props.onDecrement}
+              onDelete={this.props.onDelete}
+            />
+          ))}
+        </ul>
+
+        <button className="habit-reset" onClick={this.props.onReset}>
+          rset all
+        </button>
+      </div>
     );
   }
 }
