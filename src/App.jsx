61e9b9babd4fcb7323a5d84bd0c63965c8ed1d89
Jinnie Cho <yejincho1105@gmail.com>
2022-02-25 00:58:12 +0900

    💄 Styled App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import "./App.css";
+import GNB from "./components/GNB";
 import HabitList from "./components/HabitList";
 
 export default class App extends Component {
@@ -35,14 +35,29 @@ export default class App extends Component {
 
   render() {
     return (
-      <>
+      <div
+        className="App 
+        flex flex-wrap flex-col md:flex-row
+        justify-start items-start
+        px-4 md:px-12
+        w-screen h-full
+        font-sans font-normal lowercase
+        text-zinc-300 bg-zinc-900
+        antialiased subpixel-antialiased "
+      >
+        <GNB
+          totalCounts={
+            this.state.habitItemInfos.filter((item) => item.count > 0).length
+          }
+        />
+
         <HabitList
           habitItemInfos={this.state.habitItemInfos}
           onIncrement={this.handleIncrement}
           onDecrement={this.handleDecrement}
           onDelete={this.handleDelete}
         />
-      </>
+      </div>
     );
   }
 }
