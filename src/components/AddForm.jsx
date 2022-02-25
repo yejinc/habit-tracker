2aa624b18c9bafe8a3237978358b2422d4777a58
Jinnie Cho <yejincho1105@gmail.com>
2022-02-25 17:39:06 +0900

    🏷️ Updated AddForm component from Component to PureComponent

--- src/components/AddForm.jsx
+++ src/components/AddForm.jsx
@@ -1,6 +1,6 @@
-import React, { Component } from "react";
+import React, { PureComponent } from "react";
 
-export default class AddForm extends Component {
+export default class AddForm extends PureComponent {
   formRef = React.createRef();
   inputRef = React.createRef();
 
